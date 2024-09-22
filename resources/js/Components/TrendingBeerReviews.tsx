702fc9447cd1e2b099c0c105d7aa817c835edfe9
Chris <cbrosnan00@gmail.com>
2024-09-22 21:34:47 +0100

    Update TrendingBeerReviews.tsx

--- resources/js/Components/TrendingBeerReviews.tsx
+++ resources/js/Components/TrendingBeerReviews.tsx
@@ -1,42 +1,91 @@
+import { useEffect, useState } from 'react';
+import { Link } from '@inertiajs/react';
+
+interface Beverage {
+    id: string;
+    title: string;
+    date: string;
+    featuredImage: {
+        node: {
+            sourceUrl: string;
+            title: string;
+            caption: string;
+        }
+    }
+}
+
 export default function TrendingBeerReviews() {
+    const wp_graph_ql_query = `query RecentBeverages {
+        allBeverage(first: 5) {
+            nodes {
+            content
+            slug
+            status
+            title
+            date
+            databaseId
+            featuredImage {
+                    node {
+                    sourceUrl
+                    title
+                    mediaItemUrl
+                    caption
+                    uri
+                    }
+                }
+            id
+            }
+        }
+    }`;
+
+    const fetch_beer_reviews = async () => {
+        const response = await fetch('https://pintglassldn.com/cms/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ query: wp_graph_ql_query }),
+        });
+        const data      = await response.json();
+        const beverages = data.data.allBeverage.nodes;
+        return beverages;
+    }
+
+    const [beverages, setBeverages] = useState<Beverage | null>(null);
+
+    useEffect(() => {
+        fetch_beer_reviews().then((beverages) => {
+            setBeverages(beverages);
+        });
+    }, []);
+
+    const beverages_array = beverages ? beverages : [];
+
     return (
         <div>
-            <div className="grid gap-6 lg:grid-cols-4 lg:gap-8 lg:px-12 pb-4">
+            <div className="lg:px-12 pb-4">
                 <h2 className="font-black text-lg">Trending Beer Reviews</h2>
             </div>
-            <div className="grid gap-6 lg:grid-cols-4 lg:gap-8 lg:px-12">
-                <a href={route('blog.index')} >
-                    <div className="flex items-center justify-between pb-3">
-                        <img src="https://via.placeholder.com/800x400" alt="Screenshot of the application" className="w-full h-full object-cover rounded-lg shadow-lg" />
-                    </div>
-                    <h2 className="font-black pb-3">Recent Post</h2>
-                    <p className="pb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua....</p>
-                    <button className="bg-gray-400 hover:bg-amber-200 hover:text-amber-900 text-white font-bold p-2 text-xs rounded">Read More</button>
-                </a>
-                <a href={route('blog.index')} >
-                    <div className="flex items-center justify-between pb-3">
-                        <img src="https://via.placeholder.com/800x400" alt="Screenshot of the application" className="w-full h-full object-cover rounded-lg shadow-lg" />
-                    </div>
-                    <h2 className="font-black pb-3">Recent Post</h2>
-                    <p className="pb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua....</p>
-                    <button className="bg-gray-400 hover:bg-amber-200 hover:text-amber-900 text-white font-bold p-2 text-xs rounded">Read More</button>
-                </a>
-                <a href={route('blog.index')} >
-                    <div className="flex items-center justify-between pb-3">
-                        <img src="https://via.placeholder.com/800x400" alt="Screenshot of the application" className="w-full h-full object-cover rounded-lg shadow-lg" />
-                    </div>
-                    <h2 className="font-black pb-3">Recent Post</h2>
-                    <p className="pb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua....</p>
-                    <button className="bg-gray-400 hover:bg-amber-200 hover:text-amber-900 text-white font-bold p-2 text-xs rounded">Read More</button>
-                </a>
-                <a href={route('blog.index')} >
-                    <div className="flex items-center justify-between pb-3">
-                        <img src="https://via.placeholder.com/800x400" alt="Screenshot of the application" className="w-full h-full object-cover rounded-lg shadow-lg" />
-                    </div>
-                    <h2 className="font-black pb-3">Recent Post</h2>
-                    <p className="pb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua....</p>
-                    <button className="bg-gray-400 hover:bg-amber-200 hover:text-amber-900 text-white font-bold p-2 text-xs rounded">Read More</button>
-                </a>
+            <div className="grid gap-5 lg:grid-cols-5 lg:gap-8 lg:px-12">
+                {
+                    // @ts-ignore
+                    beverages_array.map((beverage, index) => {
+                        return (
+                            <div key={index} className="bg-white rounded-lg shadow-lg overflow-hidden">
+                                <Link  href={route('beverages.singular', { type: 'lager', slug: beverage.slug })}>
+                                    <img src={beverage.featuredImage.node.sourceUrl} alt={beverage.featuredImage.node.title} className="w-full object-cover object-center" />
+                                </Link>
+                                <div className="p-6">
+                                    <h3 className="text-lg font-semibold text-gray-800">{beverage.title}</h3>
+                                    <p className="text-gray-600 my-3">{beverage.content.substring(0,75).replace('<p>', '')}...</p>
+                                    <Link href={route('beverages.singular', { type: 'lager', slug: beverage.slug })}>
+                                        <button className="bg-gray-400 hover:bg-amber-200 hover:text-amber-900 text-white font-bold p-2 text-xs rounded">Read More</button>
+                                    </Link>
+                                </div>
+                            </div>
+                        );
+                    })
+                }
             </div>
         </div>
     );
