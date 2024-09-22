f6f14868ad6d0752e7926890291cc9a07cc61ffb
Chris <cbrosnan00@gmail.com>
2024-09-22 20:23:02 +0100

    Update Beverage.tsx

--- resources/js/Pages/Beverages/Types/Beverage/Beverage.tsx
+++ resources/js/Pages/Beverages/Types/Beverage/Beverage.tsx
@@ -83,8 +83,8 @@ export default function Beverage({ auth, appName, slug }: PageProps<{ appName: s
                     <div className="relative w-full">
                         <Header auth={auth} appName={appName} />
 
-                        <main className="px-12 py-6">
-                            <h1 className="text-4xl font-bold text-center py-4">{ beverage?.title }</h1>
+                        <main className="px-12 pb-6">
+                            <h1 className="text-4xl font-bold text-center py-6">{ beverage?.title }</h1>
                             <div className="grid gap-5 lg:grid-cols-2 lg:gap-2 lg:px-12">
                                 <img src={ beverage?.featuredImage.node.sourceUrl } alt={ beverage?.featuredImage.node.title } className="w-full h-full object-cover rounded-lg shadow-lg" />
                                 <div className="px-4">
