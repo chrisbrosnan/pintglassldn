4f95bc1c817e0ceb3f01cc73d72b17b8f07d9a89
Chris <cbrosnan00@gmail.com>
2024-09-29 18:38:38 +0100

    Update TrendingBeerReviews.tsx

--- resources/js/Components/TrendingBeerReviews.tsx
+++ resources/js/Components/TrendingBeerReviews.tsx
@@ -15,8 +15,8 @@ interface Beverage {
 }
 
 export default function TrendingBeerReviews() {
-    const wp_graph_ql_query = `query RandomBeverages($where: RootQueryToPostConnectionWhereArgs!) {
-        RandomBeverages(first: 5, where: $where) {
+    const wp_graph_ql_query = `query TrendingBeverages {
+        allBeverage(first: 5) {
             nodes {
                 content
                 slug
@@ -62,7 +62,24 @@ export default function TrendingBeerReviews() {
         });
     }, []);
 
+    function shuffle(array : any) {
+        let currentIndex = array.length;
+
+        // While there remain elements to shuffle...
+        while (currentIndex != 0) {
+
+          // Pick a remaining element...
+          let randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+
+          // And swap it with the current element.
+          [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+        }
+    }
+
     const beverages_array = beverages ? beverages : [];
+    shuffle(beverages_array);
 
     return (
         <div>
